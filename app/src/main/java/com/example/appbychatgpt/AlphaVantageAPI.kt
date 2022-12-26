2cc94998d152c395d5b3c4f362c34781f21ada7c
Max Kachinkin <makzimi@gmail.com>
2022-12-26 23:47:24 +0300

    Add new endpoint to AlphaVantageAPI

--- app/src/main/java/com/example/appbychatgpt/AlphaVantageAPI.kt
+++ app/src/main/java/com/example/appbychatgpt/AlphaVantageAPI.kt
@@ -2,7 +2,6 @@ package com.example.appbychatgpt
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface AlphaVantageAPI {
   companion object {
@@ -10,10 +9,7 @@ interface AlphaVantageAPI {
     const val API_KEY = "YOUR_API_KEY_HERE"
   }
 
-  @GET("query")
-  fun getStockPrices(
-    @Query("function") function: String = "GLOBAL_QUOTE",
-    @Query("symbol") symbol: String,
-    @Query("apikey") apiKey: String = API_KEY
-  ): Call<StockResponse>
+
+  @GET("/query?function=TOP_100")
+  fun getTopStocks(): Call<StockResponse>
 }
