dc643c9474aaefc4aedbcb60be2e1e031e4ec337
Max Kachinkin <makzimi@gmail.com>
2022-12-26 17:48:46 +0300

    Fix Stock data class

--- app/src/main/java/com/example/appbychatgpt/Stock.kt
+++ app/src/main/java/com/example/appbychatgpt/Stock.kt
@@ -3,35 +3,9 @@ package com.example.appbychatgpt
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-class Stock {
+data class Stock(
   @SerializedName("01. symbol")
-  @Expose
-  val symbol: String? = null
-  @SerializedName("02. open")
-  @Expose
-  val open: String? = null
-  @SerializedName("03. high")
-  @Expose
-  val high: String? = null
-  @SerializedName("04. low")
-  @Expose
-  val low: String? = null
+  val name: String,
   @SerializedName("05. price")
-  @Expose
-  val price: String? = null
-  @SerializedName("06. volume")
-  @Expose
-  val volume: String? = null
-  @SerializedName("07. latest trading day")
-  @Expose
-  val latestTradingDay: String? = null
-  @SerializedName("08. previous close")
-  @Expose
-  val previousClose: String? = null
-  @SerializedName("09. change")
-  @Expose
-  val change: String? = null
-  @SerializedName("10. change percent")
-  @Expose
-  val changePercent: String? = null
-}
+  val price: String
+)
